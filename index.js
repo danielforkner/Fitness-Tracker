effdd4b489f2a5c8330635a44d1edb9f3870c4bb
Jacob <your_email@real_email.edu>
2022-04-05 10:51:18 -0500

    connect to database

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // create the express server here
+const PORT = 3000
+
+const express = require("express")
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+})
+
