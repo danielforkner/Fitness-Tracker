da2be6bd360384cc6c2c98848aa4c1a562719edf
Jacob <your_email@real_email.edu>
2022-04-07 08:55:35 -0500

    starting to build routes

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 const PORT = 3000
 
 const express = require("express")
+const cors = require('cors')
 const app = express()
+const apiRouter = require("./api")
+
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
