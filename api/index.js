c35430abe68f23541e0ed6983fc46d0c76663cc6
Jacob <your_email@real_email.edu>
2022-04-07 14:27:27 -0500

    working on create activity route

--- api/index.js
+++ api/index.js
@@ -4,12 +4,15 @@
 const express = require('express');
 const apiRouter = express.Router();
 const usersRouter = require('./users');
+const activitiesRouter = require('./activities')
 
 apiRouter.get('/health', (req, res, next) => {
   res.send({ message: 'Server is listening...' });
 });
 
 apiRouter.use('/users', usersRouter);
+
+apiRouter.use("/activities", activitiesRouter)
 
 apiRouter.use((error, req, res, next) => {
   res.send(error);
